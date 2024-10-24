68c2a5541611ba6fb4d3652f1674d2bcd08b15e7
Ardhi Lukianto <ardhi@lukianto.com>
2024-10-25 02:47:50 +0700

    Updates

--- bajo/method/route-path.js
+++ bajo/method/route-path.js
@@ -1,14 +1,16 @@
 function routePath (name) {
   const { breakNsPath, getPlugin } = this.app.bajo
+  const { getAppPrefix } = this.app.waibu
   const { trim } = this.app.bajo.lib._
 
   const { ns, path, subNs } = breakNsPath(name)
   const plugin = getPlugin(ns)
-  let fullPath = `${this.config.prefix}/${subNs}/${path}`
+  const prefix = getAppPrefix(this.name)
+  let fullPath = `${prefix}/${subNs}/${path}`
   if (subNs === 'doc') {
-    fullPath = `${this.config.prefix}/${this.config.page.prefixDocs}/${plugin.alias}/${path}`
+    fullPath = `${prefix}/${this.config.page.prefixDocs}/${plugin.alias}/${path}`
   } else if (subNs.startsWith('virtual')) {
-    fullPath = `${this.config.prefix}/${subNs.slice(7)}` +
+    fullPath = `${prefix}/${subNs.slice(7)}` +
       `/${this.app.waibu.config.prefixVirtual}/${plugin.alias}/${path}`
   }
   return `/${trim(fullPath.replaceAll('//', '/'), '/')}`
