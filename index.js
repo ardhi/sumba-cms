8dbfe3abe2ae4a0787af364172791139bb7ca406
Ardhi Lukianto <ardhi@lukianto.com>
2025-09-28 21:24:10 +0700

    Updates

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+/**
+ * Plugin factory
+ *
+ * @param {string} pkgName - NPM package name
+ * @returns {class}
+ */
 async function factory (pkgName) {
   const me = this
 
+  /**
+   * SumbaCms class
+   *
+   * @class
+   */
   class SumbaCms extends this.app.pluginClass.base {
     static alias = 'cms'
     static dependencies = ['waibu-mpa']
